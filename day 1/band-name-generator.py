769736e8ccf8ea6c6158c8950e310862bbd57bd9
dgsamper <dgsamper@id.uff.br>
2023-12-22 17:36:23 -0300

    Add day 1

---
+++ day 1/band-name-generator.py
@@ -0,0 +1,9 @@
+print("Welcome to the band name generator!")
+
+user_city = input("What city did you grow up?\n")
+user_pet_name = input("What's the name of yout pet?\n")
+
+generated_band_name = user_city + " " + user_pet_name
+
+print(f"Your band name could be {generated_band_name}")
+
---
+++ day 1/docs.md
@@ -0,0 +1,3 @@
+***DAY 1 21/12/2023***
+
+- \n to jump a line in print() and input()
